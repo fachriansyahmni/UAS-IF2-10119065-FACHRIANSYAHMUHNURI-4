ad79f0512c0000d7ab732350d73ca4b77fc12f4e
fachriansyahmni <fachriansyahmni@gmail.com>
2021-02-20 16:19:02 +0700

    upload

--- src/uas/if2/pkg10119065/fachriansyahmuhnuri/pkg4/UASIF210119065FACHRIANSYAHMUHNURI4.java
+++ src/uas/if2/pkg10119065/fachriansyahmuhnuri/pkg4/UASIF210119065FACHRIANSYAHMUHNURI4.java
@@ -5,9 +5,14 @@
  */
 package uas.if2.pkg10119065.fachriansyahmuhnuri.pkg4;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Fachriansyah PC
+ * NIM : 10119065
+ * NAMA : FACHRIANSYAH MUH NUR I
+ * KELAS : IF2
+ * NOMOR SOAL : 4
  */
 public class UASIF210119065FACHRIANSYAHMUHNURI4 {
 
@@ -16,6 +21,26 @@ public class UASIF210119065FACHRIANSYAHMUHNURI4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+          Scanner sc = new Scanner(System.in);
+          String nama,kwn, tujuan;
+          
+          System.out.println("APLIKASI PENDATAAN WISATAWAN");
+          System.out.println("============================");
+          System.out.print("Nama : ");
+          nama = sc.nextLine();
+          
+          System.out.print("Kewarganegaranan : ");
+          kwn = sc.nextLine();
+          
+          System.out.print("Tujuan Wisata : ");
+          tujuan = sc.nextLine();
+          
+          System.out.println();
+          System.out.println("HASIL DATA APLIKASI PENDATAAN WISATAWAN");
+          System.out.println("============================");
+          System.out.println("Nama             : " + nama);
+          System.out.println("Kewarganegaranan : " + kwn);
+          System.out.println("Tujuan Wisata    : " + tujuan);
     }
     
 }
